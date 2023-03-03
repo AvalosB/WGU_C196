34bfe86937cdcc91535f14191dfec70d454e1221
Avalos-B <brianavalos0406@Gmail.com>
2023-03-02 22:29:29 -0700

    First

---
+++ app/src/main/java/com/example/brianavalos/UI/Term.java
@@ -0,0 +1,16 @@
+package com.example.brianavalos.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.brianavalos.R;
+
+public class Term extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term);
+    }
+}
